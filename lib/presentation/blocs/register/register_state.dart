2db3754ffbd07e31f0ace7a751687af079c30ae8
Jose Llanas <josellanasmota86@gmail.com>
2023-08-10 20:17:56 +0200

    use customised inputs

--- lib/presentation/blocs/register/register_state.dart
+++ lib/presentation/blocs/register/register_state.dart
@@ -3,32 +3,36 @@ part of 'register_cubit.dart';
 enum FormStatus { invalid, vaid, validating, posting }
 
 class RegisterFormState extends Equatable {
+  
   final FormStatus formStatus;
-  final String username;
+  final bool isValid;
+  final Username username;
   final String email;
   final String password;
 
   const RegisterFormState({
     this.formStatus = FormStatus.invalid,
-    this.username = '',
+    this.isValid = false,
+    this.username = const Username.pure(),
     this.email = '',
     this.password = '',
   });
 
   RegisterFormState copyWith({
     FormStatus? formStatus,
-    String? username,
+    bool? isValid,
+    Username? username,
     String? email,
     String? password,
   }) =>
       RegisterFormState(
         formStatus: formStatus ?? this.formStatus,
+        isValid: isValid ?? this.isValid,
         username: username ?? this.username,
         email: email ?? this.email,
         password: password ?? this.password,
       );
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [formStatus, isValid, username, email, password];
 }
-
