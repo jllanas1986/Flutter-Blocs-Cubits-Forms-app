d4c8d7152b2a072396f79971e3c5729e47688091
Jose Llanas <josellanasmota86@gmail.com>
2023-08-10 14:20:08 +0200

    register form cubit

---
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(const RegisterFormState());
+
+  void onSubmit() {
+    print('Submit: $state');
+  }
+
+  void usernameChange(String value) {
+    emit(state.copyWith(
+      username: value,
+    ));
+  }
+
+  void emailChange(String value) {
+    emit(state.copyWith(
+      email: value,
+    ));
+  }
+
+  void passwordChange(String value) {
+    emit(state.copyWith(
+      password: value,
+    ));
+  }
+}
