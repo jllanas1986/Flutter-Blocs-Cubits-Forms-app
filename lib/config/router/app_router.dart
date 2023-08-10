28b651ec12def91f40aafe4fd464a74a2be68d02
Jose Llanas <josellanasmota86@gmail.com>
2023-08-10 10:59:32 +0200

    register file created

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:forms_app/presentation/screens/bloc_counter_screen.dart';
 import 'package:forms_app/presentation/screens/cubit_counter_screen.dart';
 import 'package:forms_app/presentation/screens/home_screen.dart';
+import 'package:forms_app/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(routes: [
@@ -17,6 +18,11 @@ final appRouter = GoRouter(routes: [
   GoRoute(
     path: '/counter-bloc',
     builder: (context, state) => const BlocCouterScreen(),
+  ),
+
+  GoRoute(
+    path: '/new-user',
+    builder: (context, state) => const RegisterScreen(),
   ),
 
 ]);
