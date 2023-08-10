b9cbff66c182d95e755dcdc89c2f9c4ddf2cf13f
Jose Llanas <josellanasmota86@gmail.com>
2023-08-10 12:04:02 +0200

    design text field

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:forms_app/presentation/widgets/inputs/custom_text_form_field.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -19,34 +20,50 @@ class _RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.symmetric(horizontal: 10),
         child: SingleChildScrollView(
           child: Column(
             //mainAxisAlignment: MainAxisAlignment.end,
             children: [
 
-              const FlutterLogo(size: 100),
+              FlutterLogo(size: 100),
 
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
-              TextFormField(),
+              _RegisterForm(),
 
-              const SizedBox(height: 20),
-
-              FilledButton.tonalIcon(
-                onPressed: () {},
-                icon: const Icon(Icons.save),
-                label: const Text('Crear usuario')
-              ),
-
-              const SizedBox(height: 20),
+              SizedBox(height: 20),
             ]
           ),
         ),
       )
     );
+  }
+}
+
+class _RegisterForm extends StatelessWidget {
+  const _RegisterForm();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+
+          CustomTextFormField(),
+          const SizedBox(height: 10),
+          
+          CustomTextFormField(),
+
+          const SizedBox(height: 20),
+
+          FilledButton.tonalIcon(
+            onPressed: () {},
+            icon: const Icon(Icons.save),
+            label: const Text('Crear usuario')
+          ),
+
+        ],
+      ));
   }
 }
